839f84e58c655e35fe3e8d8dea41c50098c1a182
parth.d <paarth.dandiwala@gmail.com>
2015-02-01 00:12:51 +0530

    A simple representation of graph in form of list of lists in python

---
+++ graph_algorithms/graph_representation/list_of_lists_graph.py
@@ -0,0 +1,55 @@
+def BinaryTree(r):
+	return [r, [], []]
+
+def insertLeft(root,new_branch):
+	t = root.pop(1)
+
+	if len(t) > 1:
+		root.insert(1,[new_branch,t,[]])
+	else:
+		root.insert(1,[new_branch, [], []])
+
+	return root
+
+def insertRight(root,new_branch):
+	t = root.pop(2)
+
+	if len(t) > 1:
+		root.insert(2, [new_branch, [], t])
+	else:
+		root.insert(2, [new_branch, [], []])
+
+	return root
+
+def getRootVal(root):
+	return root[0]
+
+def setRootVal(root,new_value):
+	root[0] = new_value
+
+def getLeftChild(root):
+	return root[1]
+
+def getRightChild(root):
+	return root[2]
+
+## Test to simluate and check the implementation 
+# r = BinaryTree(3)
+# insertLeft(r,4)
+# insertLeft(r,5)
+# insertRight(r,6)
+# insertRight(r,7)
+# l = getLeftChild(r)
+# print(l)
+
+# setRootVal(l,9)
+# print(r)
+# insertLeft(l,11)
+# print(r)
+# print(getRightChild(getRightChild(r)))
+
+## Sample Result to tally
+# [5, [4, [], []], []]
+# [3, [9, [4, [], []], []], [7, [], [6, [], []]]]
+# [3, [9, [11, [4, [], []], []], []], [7, [], [6, [], []]]]
+# [6, [], []]
